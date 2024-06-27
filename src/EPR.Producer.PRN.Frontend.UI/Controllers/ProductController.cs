694fc15c1d8514525e3a36d486559358059905ea
Stephen Williamson (ext) <stephen.williamson.external@eviden.com>
2024-06-27 12:32:57 +0100

    Create empty mvc and unit test projects

---
+++ src/EPR.Producer.PRN.Frontend.UI/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EPR.Producer.PRN.Frontend.UI.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
